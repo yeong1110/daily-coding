6434a56505a92eb9527ec70dc8634ea927992694
yeong1110 <a0257942@gmail.com>
2026-01-21 20:57:10 +0900

    DAY3 수정

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
 	const userName = prompt('사용자명을 입력해주세요');
-	console.log(`환영합니다  ${userName}님!`);
-	alert(`환영합니다  ${userName}님!`)
+	
+	if(userName && userName.trim()) {
+		const message = `환영합니다  ${userName}님!`
+		console.log(message);
+		alert(message);
+	}
 })
