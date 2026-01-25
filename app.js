ea089224ddb5554791d7bdc9285a1306d1dcf719
yeong1110 <a0257942@gmail.com>
2026-01-25 13:48:25 +0900

    merge with DAY6

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', () => {
 	let userName = prompt('사용자명을 입력해주세요');
 	
-	while(userName === null || !userName) {
+	while(userName !== null && !userName.trim()) {
 		userName = prompt('사용자명은 필수값입니다')
 	}
-	if(userName && userName.trim()) {
+	if(userNamegit) {
 		const message = `환영합니다 ${userName}님!`
 		console.log(message);
 		alert(message);
